8c886e25a94f82ae5562d55765b86670d5a186b7
roser-rsr <ruizrojas1203@gmail.com>
2025-05-16 22:32:48 +0200

    "Afegida funcionalitat per mostrar contactes"

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // script.js
 
 console.log("Agenda web");
+const contactes = ["Roser","Saida","Ruben"];
+const llista = document.createElement('ul');
+contactes.forEach(nom=>{
+	const li = document.createElement('li');
+	li.textContent = nom;
+	llista.appendChild(li);
+});
+document.body.appendChild(llista);
