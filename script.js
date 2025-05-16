a43f4bb62dd7400f3c9783486f2900a9c2ab4123
Marta <marta@exemple.com>
2025-05-16 23:03:36 +0200

    "Control d'errors per camps buits al formulari"

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ contactes.forEach(nom=>{
 	llista.appendChild(li);
 });
 document.body.appendChild(llista);
+
+document.querySelector("form").addEventListener("submit", e=>{
+ e.preventDefault();
+const input = document.querySelector("#nom");
+if (input.value.trim() === "") {
+alert ("El camp no pot estar buit");
+}
+//Lògica per afegir el contacte...
+});
